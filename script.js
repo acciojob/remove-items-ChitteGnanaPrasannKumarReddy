4fc6fe7a7cb05c4c02372fbed268b1988d26b4a8
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-13 22:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;
+
+    // Check if a color is selected
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+}
